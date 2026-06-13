583bfbd5b5ad303fee71e8c2c506ed74d55b67a0
aethercoreinteractive-jpg <aethercoreinteractive@gmail.com>
2026-06-13 21:54:45 +0530

    Update data.js

--- data.js
+++ data.js
@@ -79,7 +79,7 @@ const aetherCoreApps = [
         category: "Student Utilities", 
         title: "Scientific Calci", 
         logo: "ScientificLogo.png", 
-        description: "Solve complex equations and master technical precision with Scientific Calci.\n\nIdeal for:\n• STEM Professionals: Handling advanced calculus, statistical modeling, and intricate engineering equations.\n• Students: Mastering physics, chemistry, and mathematics with reliable, high-speed computation for assignments and exams.\n• Technical Projects: Streamlining unit conversions, data analysis, and professional-grade measurements.", 
+        description: "Solve complex equations and master technical precision with Scientific Calci (Scientific Calculator).\n\nIdeal for:\n• STEM Professionals: Handling advanced calculus, statistical modeling, and intricate engineering equations.\n• Students: Mastering physics, chemistry, and mathematics with reliable, high-speed computation for assignments and exams.\n• Technical Projects: Streamlining unit conversions, data analysis, and professional-grade measurements.", 
         preview: ["Scientific-1.png", "Scientific-calculator.jpg"], 
         url: "https://sharanmusician.com/Scientific-calculator-/",
         rating: "4/5"
