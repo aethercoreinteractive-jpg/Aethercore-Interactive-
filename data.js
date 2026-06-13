4e94439b41d247ffb76f1d7ccea4070549626a45
aethercoreinteractive-jpg <aethercoreinteractive@gmail.com>
2026-06-13 20:40:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const aetherCoreApps = [
     category: "Editors Choice", 
     title: "Page Forge", 
     logo: "Pageforge-Logo.jpg", 
-    description: "Transform your images into professional PDF documents with Page Forge.\n\nIdeal for:\n• Work & Business: Scanning invoices, receipts, and contracts.\n• Students: Digitizing notes, assignments, and study materials.\n• Personal Use: Archiving IDs, medical documents, or creating digital albums.", 
+    description: "Transform your images into professional PDF documents (Image to PDF) with Page Forge.\n\nIdeal for:\n• Work & Business: Scanning invoices, receipts, and contracts.\n• Students: Digitizing notes, assignments, and study materials.\n• Personal Use: Archiving IDs, medical documents, or creating digital albums.", 
     preview: ["Pageforge-1.png", "Pageforge-2.png", "Pageforge-3.png", "Pageforge-4.png", "Pageforge-5.png"], 
     url: "https://sharanmusician.com/Page-Forge/",
     rating: "5/5"
